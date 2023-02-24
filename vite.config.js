dccf33a6b969462080b9ae3e7de16a4bbaa3ea11
Alex Martin <alextravc.342@gmail.com>
2023-02-24 11:04:48 -0500

    [ADD]: init structure

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+/* eslint-disable no-undef */
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// // // Path
+import * as path from 'path';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
+  },
+});
