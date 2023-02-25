c8353631e380a12c43108fda5e140845b7d97c1f
Alex Martin <alextravc.342@gmail.com>
2023-02-25 14:50:15 -0500

    [ADD]: geProducts RTK

--- src/store/api/apiSlice.js
+++ src/store/api/apiSlice.js
@@ -9,16 +9,20 @@ export const api = createApi({
 
   baseQuery: fetchBaseQuery({ baseUrl: VITE_API_URL }),
 
-  
-  tagTypes: ['User'],
+  tagTypes: ['User', 'Products'],
   endpoints: build => ({
     getUser: build.query({
       query: id => `/clients/${id}`,
       providesTags: ['User'],
+    }),
+    getProducts: build.query({
+      query: () => '/products',
+      providesTags: ['Products'],
     }),
   }),
 });
 
 export const {
   useGetUserQuery, // <-- endpoints
+  useGetProductsQuery,
 } = api;
