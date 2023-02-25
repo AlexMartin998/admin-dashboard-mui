307cd9556efdf62d91dcbf48fdb702361ab7633b
Alex Martin <alextravc.342@gmail.com>
2023-02-25 15:52:40 -0500

    [ADD]: product card component

--- src/admin/scenes/products/Products.jsx
+++ src/admin/scenes/products/Products.jsx
@@ -1,94 +1,8 @@
-import { useState } from 'react';
-import {
-  Box,
-  Card,
-  CardActions,
-  CardContent,
-  Collapse,
-  Button,
-  Typography,
-  Rating,
-  useTheme,
-  useMediaQuery,
-  CircularProgress,
-} from '@mui/material';
+import { Box, CircularProgress, useMediaQuery } from '@mui/material';
 
 import { useGetProductsQuery } from '../../../store';
 import { Header } from '../../shared';
-
-const Product = ({
-  _id,
-  name,
-  price,
-  description,
-  category,
-  rating,
-  supply,
-  stat,
-}) => {
-  const theme = useTheme();
-  const [isExpanded, setIsExpanded] = useState(false);
-
-  return (
-    <Card
-      sx={{
-        backgroundImage: 'none',
-        backgroundColor: theme.palette.background.alt,
-        borderRadius: '0.55rem',
-      }}
-    >
-      <CardContent>
-        <Typography
-          sx={{ fontSize: '14px' }}
-          color={theme.palette.secondary[700]}
-          gutterBottom
-        >
-          {category}
-        </Typography>
-        <Typography variant="h5" component="div">
-          {name}
-        </Typography>
-        <Typography sx={{ mb: '1.5rem' }} color={theme.palette.secondary[400]}>
-          ${Number(price).toFixed(2)}
-        </Typography>
-
-        <Rating value={rating} readOnly />
-
-        <Typography variant="body2">{description}</Typography>
-      </CardContent>
-
-      <CardActions>
-        <Button
-          variant="primary"
-          size="small"
-          onClick={() => setIsExpanded(!isExpanded)}
-        >
-          See More
-        </Button>
-      </CardActions>
-
-      <Collapse
-        in={isExpanded}
-        timeout="auto"
-        unmountOnExit
-        sx={{
-          color: theme.palette.neutral[300],
-        }}
-      >
-        <CardContent>
-          <Typography>id: {_id}</Typography>
-          <Typography>Supply Left: {supply}</Typography>
-          <Typography>
-            Yearly Sales This Year: {stat.yearlySalesTotal}
-          </Typography>
-          <Typography>
-            Yearly Units Sold This Year: {stat.yearlyTotalSoldUnits}
-          </Typography>
-        </CardContent>
-      </Collapse>
-    </Card>
-  );
-};
+import ProductCard from './ProductCard';
 
 const Products = () => {
   const { data, isLoading } = useGetProductsQuery();
@@ -106,13 +20,13 @@ const Products = () => {
           justifyContent="space-between"
           rowGap="20px"
           columnGap="1.33%"
-          // select any child component of this box:
+          // select any div child component of this box:
           sx={{
             '& > div': { gridColumn: isNonMobile ? undefined : 'span 4' },
           }}
         >
           {data.map(product => (
-            <Product
+            <ProductCard
               key={product._id}
               {...{ ...product, stat: product.stat[0] }}
             />
