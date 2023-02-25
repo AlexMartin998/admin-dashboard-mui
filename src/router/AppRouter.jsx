26d0e09681cc98225502402f58fee30b0ed1ea1b
Alex Martin <alextravc.342@gmail.com>
2023-02-25 15:43:24 -0500

    [ADD]: products page

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -2,13 +2,14 @@ import { Navigate } from 'react-router-dom';
 import { Route, Routes } from 'react-router-dom';
 
 import { AdminLayout } from '../admin/layout';
-import { Dashboard } from '../admin/scenes';
+import { Dashboard, Products } from '../admin/scenes';
 
 const AppRouter = () => {
   return (
     <Routes>
-      <Route path="/dashboard" element={<AdminLayout />}>
-        <Route index element={<Dashboard />} />
+      <Route path="/" element={<AdminLayout />}>
+        <Route path="dashboard" element={<Dashboard />} />
+        <Route path="products" element={<Products />} />
 
         <Route path="*" element={<Navigate to="/dashboard" replace />} />
       </Route>
