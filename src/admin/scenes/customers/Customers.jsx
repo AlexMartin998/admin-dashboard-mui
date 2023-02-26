803b92617725417dabe7238f6e8d4b98088659e8
Alex Martin <alextravc.342@gmail.com>
2023-02-25 22:23:26 -0500

    [ADD]: columsStructure memorized

--- src/admin/scenes/customers/Customers.jsx
+++ src/admin/scenes/customers/Customers.jsx
@@ -1,30 +1,16 @@
 import { Box, useTheme } from '@mui/material';
 import { DataGrid } from '@mui/x-data-grid';
+import { useMemo } from 'react';
 import { useGetCustomersQuery } from '../../../store';
 
 import { Header } from '../../shared';
+import { columnsStructure } from './colums';
 
 const Customers = () => {
   const theme = useTheme();
   const { data, isLoading } = useGetCustomersQuery();
 
-  const columns = [
-    { field: 'id', headerName: 'ID', flex: 1 },
-    { field: 'name', headerName: 'Name', flex: 0.5 },
-    { field: 'email', headerName: 'Email', flex: 1 },
-
-    {
-      field: 'phoneNumber',
-      headerName: 'Phone Number',
-      flex: 0.5,
-      renderCell: params => {
-        return params.value.replace(/^(\d{3})(\d{3})(\d{4})/, '($1) $2 - $3');
-      },
-    },
-    { field: 'country', headerName: 'Country', flex: 0.4 },
-    { field: 'occupation', headerName: 'Occupation', flex: 1 },
-    { field: 'role', headerName: 'Role', flex: 0.5 },
-  ];
+  const columns = useMemo(() => columnsStructure, []);
 
   return (
     <Box m="1.5rem 2.5rem">
