324c7d9b9f9437a54cc63b4957931c5a0ffebfd8
Alex Martin <alextravc.342@gmail.com>
2023-02-27 08:01:48 -0500

    [ADD]: nivo-line overview chart

--- src/admin/shared/index.js
+++ src/admin/shared/index.js
@@ -1,3 +1,4 @@
 export { default as Header } from './components/Header';
 export { default as Navbar } from './components/Navbar';
+export { default as OverviewChart } from './components/OverviewChart';
 export { default as Sidebar } from './components/Sidebar';
