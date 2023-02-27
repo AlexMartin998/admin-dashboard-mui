cbc062dad619991927f2f676d066a02354b4331a
Alex Martin <alextravc.342@gmail.com>
2023-02-27 12:49:42 -0500

    [ADD]: DataGridCustomColumnMenu component

--- src/admin/shared/index.js
+++ src/admin/shared/index.js
@@ -1,5 +1,6 @@
 export { default as BreakdownChart } from './components/BreakdownChart';
 export { default as DailyOrMonthlyLine } from './components/DailyOrMonthlyLine';
+export { default as DataGridCustomColumnMenu } from './components/DataGridCustomColumnMenu';
 export { default as Header } from './components/Header';
 export { default as Navbar } from './components/Navbar';
 export { default as OverviewChart } from './components/OverviewChart';
