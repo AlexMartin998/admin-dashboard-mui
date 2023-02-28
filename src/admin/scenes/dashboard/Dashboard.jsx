8be5df7d32cd2fcd5570e9b67a24c8981b31e4dd
Alex Martin <alextravc.342@gmail.com>
2023-02-27 22:31:44 -0500

    [ADD]: getDashboard (RTK Query)

--- src/admin/scenes/dashboard/Dashboard.jsx
+++ src/admin/scenes/dashboard/Dashboard.jsx
@@ -1,7 +1,16 @@
-import React from 'react';
+import { Box } from '@mui/material';
+
+import { FlexBetween } from '../../../shared';
+import { Header } from '../../shared';
 
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  return (
+    <Box m="1.5rem 2.5rem">
+      <FlexBetween>
+        <Header title="DASHBOARD" subtitle="Welcome to your dashboard" />
+      </FlexBetween>
+    </Box>
+  );
 };
 
 export default Dashboard;
